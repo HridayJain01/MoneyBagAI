9421da49b391b5a5e1213ed5ac311af393756ff5
jay-sharmaa <sjaysharma00@gmail.com>
2026-07-28 15:22:12 +0530

    git init

---
+++ src/main/java/org/example/moneybags/MoneybagsApplication.java
@@ -0,0 +1,13 @@
+package org.example.moneybags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneybagsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneybagsApplication.class, args);
+    }
+
+}
